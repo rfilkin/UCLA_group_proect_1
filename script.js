7f21be9cd65dd9325bd74f5e91eb6be0e52ea9a0
Robert Filkin <rfilkin17@gmail.com>
2020-02-13 18:09:03 -0800

    gif grabber on buttons

--- script.js
+++ script.js
@@ -3,21 +3,23 @@ $("button").on("click", function(){
     pushedButton =$(this).text();
     displayInfo(pushedButton);
 });
+
 function displayInfo(sentButton) { 
     var api_URL = "https://sv443.net/jokeapi/v2/joke/"+sentButton;
     $.ajax({
         url: api_URL,
         method: "GET"
     }).then(function(response){
-            if(response.type==="single"){
-                $(".joke-display").text(response.joke);
-            }else{
-                var nextLine=$("<p>")
-                $(".joke-display").text(response.setup);
-                nextLine.text(response.delivery);
-                $(".joke-display").append(nextLine);
-            }
-        });
+        if(response.type==="single"){
+            $(".joke-display").text(response.joke);
+        }else{
+            var nextLine=$("<p>")
+            $(".joke-display").text(response.setup);
+            nextLine.text(response.delivery);
+            $(".joke-display").append(nextLine);
+        }
+        gif_grabber();
+    });
 }
 
 
@@ -37,9 +39,8 @@ function sample_word(joke){
     }
 }
 
-$("#programming-button").on("click", function() {
-
-    var sampled_word = sample_word("The quick brown fox jumps over the lazy dog");
+function gif_grabber(){
+    var sampled_word = sample_word($(".joke-display").text());
     console.log(sampled_word);
 
     var api_key = "XhXMrsEUUNOn44NMuFufbM8ji4bdOHdM"; //limit 42 requests per hour, 1000 requests per day
@@ -53,4 +54,38 @@ $("#programming-button").on("click", function() {
         console.log(response);
         $("#gif-display").attr("src", response.data.image_original_url);
     });
-});
+}
+
+function joke_searcher(){
+    event.preventDefault();
+    //var api_URL = "https://jokeapi-v2.p.rapidapi.com/joke/Any?api_key=fa69145befmshc39d266ba3896ddp1a470ejsndddb85d59df4&contains=C%23"
+    var api_URL = "https://icanhazdadjoke.com/j/search?term=hipster";
+    $.ajax({
+        url: api_URL,
+        method: "GET"
+    }).then(function(response){
+        console.log("looking for dad jokes");
+        console.log(response);
+        $(".joke-display").text(response.joke);
+    });
+}
+
+$("#search-btn").on("click", joke_searcher);
+
+// $("#programming-button").on("click", function() {
+
+//     var sampled_word = sample_word("The quick brown fox jumps over the lazy dog");
+//     console.log(sampled_word);
+
+//     var api_key = "XhXMrsEUUNOn44NMuFufbM8ji4bdOHdM"; //limit 42 requests per hour, 1000 requests per day
+//     var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=" + api_key +"&tag=" + sampled_word;
+//     console.log(queryURL);
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(response) {
+//         console.log(response);
+//         $("#gif-display").attr("src", response.data.image_original_url);
+//     });
+// });
